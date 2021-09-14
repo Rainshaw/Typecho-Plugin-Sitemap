276d70769b2933ff1c1429d8225c0b382ff58703
Rainshaw <rxg@live.com>
2021-09-14 15:23:33 +0800

    Update Plugin.php

--- Plugin.php
+++ Plugin.php
@@ -7,8 +7,8 @@ define('__TYPECHO_PLUGIN_SITEMAP_VERSION__', '2.0.1');
  * 
  * @package Sitemap
  * @author <strong style="color:#28B7FF;font-family: 楷体;">Rainshaw</strong>
- * @version 2.0.1
- * @dependence 18.10.23-*
+ * @version 2.1.0
+ * @since 1.2.0
  * @link https://github.com/RainshawGao
  */
 class Sitemap_Plugin implements Typecho_Plugin_Interface
